bc63c0185476e648b43fb162668758194be5afcc
Talhah Zahid <talhazahid415@gmail.com>
2025-12-23 17:49:47 +0500

    Integrate signup API with React Hook Form and Zod validation

--- src/Page/Signup.jsx
+++ src/Page/Signup.jsx
@@ -1,50 +1,72 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import z from "zod";
 const Signup = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const Cnic = useRef();
   const Email = useRef();
   const Username = useRef();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const cnic = Cnic.current.value;
-    const email = Email.current.value;
-    const userName = Username.current.value;
-    console.log(cnic);
-    console.log(email);
-    console.log(userName);
+
+  const signupSchema = z.object({
+    cnic: z.string().min(13, "CNIC must be 13 digits"),
+    email: z.string().email("Invalid email"),
+    userName: z.string().min(3, "Username too short"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      cnic: "",
+      email: "",
+      userName: "",
+    },
+  });
+
+  const onSubmits = async (data) => {
+    console.log(data);
+    setLoading(true);
     try {
-      setLoading(true);
       const response = await fetch(
-        `https://microfinance-app-backend-rho.vercel.app/api/v1/signin`,
+        "https://microfinance-app-backend-rho.vercel.app/api/v1/signin",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ cnic, email, userName }),
+          body: JSON.stringify({
+            cnic: data.cnic,
+            email: data.email,
+            userName: data.userName,
+          }),
         }
       );
-      const data = await response.json();
-      console.log(data);
+
+      const result = await response.json();
+      console.log(result);
+
       if (response.ok) {
         toast.success("Your Account Created Successfully. Check your Email.");
-        Cnic.current.value = "";
-        Email.current.value = "";
-        Username.current.value = "";
         navigate("/login");
       } else {
-        toast.error(data?.message || "Something went wrong.");
+        toast.error(result?.message || "Something went wrong");
       }
     } catch (error) {
       console.log(error);
+      toast.error("Server error");
     } finally {
       setLoading(false);
     }
   };
+
   return (
     <>
       <div className="min-h-screen py-25 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 justify-items-center sm:px-3">
@@ -77,8 +99,7 @@ const Signup = () => {
         {/* second box  */}
         <div className=" w-full flex justify-center items-center">
           <form
-            onSubmit={handleSubmit}
-            action=""
+            onSubmit={handleSubmit(onSubmits)}
             className="flex flex-col gap-3 items-center bg-white shadow-2xl  p-15 rounded-2xl w-full max-w-md"
           >
             <h1
@@ -90,22 +111,34 @@ const Signup = () => {
             </h1>
             <input
               placeholder="Enter Cnic Number"
+              id="cnic"
               type="number"
-              ref={Cnic}
+              {...register("cnic")}
               className="px-3 py-2 focus:outline-none border border-gray-300 focus:ring-indigo-700 focus:ring-2 rounded-lg w-full"
             />
+            {errors.cnic && (
+              <p className="text-red-500 text-sm">{errors.cnic.message}</p>
+            )}
             <input
               placeholder="Enter Email"
+              id="email"
               type="email"
-              ref={Email}
+              {...register("email")}
               className="px-3 py-2 focus:outline-none border border-gray-300 focus:ring-indigo-700 focus:ring-2 rounded-lg w-full"
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email.message}</p>
+            )}
             <input
               placeholder="Enter Username"
+              id="userName"
               type="text"
-              ref={Username}
+              {...register("userName")}
               className="px-3 py-2 focus:outline-none border border-gray-300 focus:ring-indigo-700 focus:ring-2 rounded-lg w-full"
             />
+            {errors.userName && (
+              <p className="text-red-500 text-sm">{errors.userName.message}</p>
+            )}
             <button className="px-9 py-2 bg-blue-700 rounded-lg text-white w-full ">
               {loading ? (
                 <Loader2 className="animate-spin h-5 w-5 mx-auto text-center " />
